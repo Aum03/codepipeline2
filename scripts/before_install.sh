27e30204c306dbb6365cbf4dc3b1d89c055079a0
Aum <aumyadav0311@gmail.com>
2023-10-09 04:05:53 +0530

    changed beforeinstall

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,7 +4,9 @@
 cd /home/ec2-user/server
 
 #_Update_&_Set_Node_Version
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+# curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+sudo yum install https://rpm.nodesource.com/pub_16.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
+sudo yum install nodejs -y --setopt=nodesource-nodejs.module_hotfixes=1
 
 #_Download_Node_&NPM
 yum -y install nodejs npm
