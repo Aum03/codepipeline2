3cf7e88d4171216c2166f8559e2e982650be8c70
Aum <aumyadav0311@gmail.com>
2023-10-09 08:50:49 +0530

    changed afterinstall

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,10 +6,10 @@ cd /home/ec2-user/server
 #_Remove_Unused_Code
 rm -rf node_modules
 rm -rf build
-sudo fallocate -l 1G /swapfile  # Create a 1GB swap file (adjust the size as needed)
-sudo chmod 600 /swapfile        # Set the correct permissions
-sudo mkswap /swapfile            # Mark the file as swap space
-sudo swapon /swapfile            # Enable swap
+# sudo fallocate -l 1G /swapfile  # Create a 1GB swap file (adjust the size as needed)
+# sudo chmod 600 /swapfile        # Set the correct permissions
+# sudo mkswap /swapfile            # Mark the file as swap space
+# sudo swapon /swapfile            # Enable swap
 
 #Install_node_modules_&_Make_React_Build
 npm  install
